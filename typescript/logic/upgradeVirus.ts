5d28798b0b9311e5eaeb218f40590c9873e3df0a
Georgi Yordanov <aaster23@protonmail.com>
2018-11-15 16:31:53 +0200

    Minor changes

--- typescript/logic/upgradeVirus.ts
+++ typescript/logic/upgradeVirus.ts
@@ -3,6 +3,9 @@ import { Virus } from '../world/virus';
 import { Player } from './../world/player';
 
 export const upgradeVirus: Function = (virus: Virus, upgrade: IUpgrade, cost: number, player: Player): void => {
+    if (player.points - cost < 0){
+        alert(`You don't have enough points to buy the upgrade!`)
+    }
     player.points = -cost;
     virus[upgrade.propertyToUpgrade] += upgrade.upgradeEffect;
 };
