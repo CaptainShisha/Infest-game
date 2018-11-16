106ae8777830a4363beb99362a357c14ded05f33
Stefan Georgiev <s.georgiev.007@gmail.com>
2018-11-16 15:44:27 +0200

    more Virus UT

--- typescript/spec/virus/virus.spec.ts
+++ typescript/spec/virus/virus.spec.ts
@@ -34,10 +34,30 @@ describe('Virus', () => {
         let test: Virus;
         beforeEach(() =>{
              test = new Virus('name');
-        });
-        it('give back the right values', () => {
-        // Arrange & Act & Assert
-        expect(test.virusName).toBe('name');
         })
-    })
+        it('give back the right name', () => {
+        // Arrange & Act & Assert
+            expect(test.virusName).toBe('name');
+        });
+
+        it('give back the right deadliness', () => {
+            // Arrange & Act & Assert
+                expect(test.deadliness).toBe(0);
+        });
+
+        it('give back the right spread', () => {
+            // Arrange & Act & Assert
+                expect(test.spread).toBe(1);
+        });
+
+        it('give back the right chanceOfMutation', () => {
+            // Arrange & Act & Assert
+                expect(test.chanceOfMutation).toBe(0);
+        });
+
+        it('give back the right chanceOfDiscovery', () => {
+            // Arrange & Act & Assert
+                expect(test.chanceOfDiscovery).toBe(0);
+        });
+    });
 });
