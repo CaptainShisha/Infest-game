2311849af385de8ce00f899f901d13f37b0745f5
Stefan Georgiev <s.georgiev.007@gmail.com>
2018-11-19 14:24:31 +0200

    infest change

--- typescript/logic/infest.ts
+++ typescript/logic/infest.ts
@@ -5,7 +5,7 @@ export const infest: Function = (continent: Continent, rate: number): number => 
     let infested: number = 0;
     if (continent.isInfected === true) {
         infested = continent.infestedPopulation + Math.round((continent.infestedPopulation * (rate + Math.random())) + 1);
-        if (continent.population < continent.infestedPopulation + infested) {
+        if (continent.population < infested) {
             infested = continent.population - continent.infestedPopulation;
         }
         continent.infestedPopulation = infested;
